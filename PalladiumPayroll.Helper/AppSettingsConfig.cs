2e8bd865ae2d95e191c0d9c0017d93859a5436ec
Kishan <kishan.bhadani@etatvasoft.com>
2025-06-12 18:14:22 +0530

    AppSetting Updated

--- PalladiumPayroll.Helper/AppSettingsConfig.cs
+++ PalladiumPayroll.Helper/AppSettingsConfig.cs
@@ -8,5 +8,16 @@ namespace PalladiumPayroll.Helper
         {
             return configuration.GetConnectionString(name: "DefaultConnection");
         }
+
+        public static T? GetSection<T>(IConfiguration configuration, string sectionName)
+        {
+            return configuration.GetSection(sectionName).Get<T>();
+        }
+
+        public static string? GetValue(IConfiguration configuration, string keyName)
+        {
+            return configuration.GetValue<string>(keyName);
+        }
+
     }
 }
