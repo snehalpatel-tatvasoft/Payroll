d6374aeda16d194d6fa8843cc5c804cfdd6e7a5f
DhvanitDodiyatatva <tatva.pci29@outlook.com>
2025-07-21 10:33:49 +0530

    Notification setup module commit

--- PalladiumPayroll.Repositories/IServiceCollectionExtension.cs
+++ PalladiumPayroll.Repositories/IServiceCollectionExtension.cs
@@ -27,10 +27,10 @@ namespace PalladiumPayroll.Repositories
             services.AddScoped<IEmployeeCodesRepository, EmployeeCodesRepository>();
             services.AddScoped<ICustomizeReportRepository, CustomizeReportRepository>();
             services.AddScoped<IPayslipDisplaySetupRepository, PayslipDisplaySetupRepository>();
-
             services.AddScoped<ITimesheetSetupRepository, TimesheetSetupRepository>();
             services.AddScoped<ICreateTransactionRepository, CreateTransactionRepository>();
             services.AddScoped<IPasswordPolicyRepository, PasswordPolicyRepository>();
+            services.AddScoped<INotificationSetupRepository, NotificationSetupRepository>();
             return services;
         }
     }
--- PalladiumPayroll.Services/IServiceCollectionExtension.cs
+++ PalladiumPayroll.Services/IServiceCollectionExtension.cs
@@ -31,6 +31,7 @@ namespace PalladiumPayroll.Services
             services.AddScoped<ITimesheetSetupService, TimesheetSetupService>();
             services.AddScoped<ICreateTransactionService, CreateTransactionService>();
             services.AddScoped<IPasswordPolicyService, PasswordPolicyService>();
+            services.AddScoped<INotificationSetupService, NotificationSetupService>();
             return services;
         }
     }
